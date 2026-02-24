d1d3484a437eb5fbf1f8a46e5c96baaecc411294
Maksss <hvcug4462@gmail.com>
2026-02-24 18:10:11 +0200

    added Subject, Teacher, SChoolClass and Student models

--- school_schedule/models.py
+++ school_schedule/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.subject}"
+    
+class SchoolClass(models.Model):
+    name = models.CharField(max_lenght=20)
+
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    school_class = models.ForeignKey(SchoolClass, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.school_class}"
